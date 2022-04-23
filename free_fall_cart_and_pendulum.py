98f6d196a91e519d1a548083f9cd02d7d3c57590
Vishikh <59769860+dungeon-master27@users.noreply.github.com>
2022-04-23 14:34:51 +0530

    Update free_fall_cart_and_pendulum.py

--- free_fall_cart_and_pendulum.py
+++ free_fall_cart_and_pendulum.py
@@ -2,14 +2,12 @@ import numpy as np
 import cv2
 from free_fall_pendulum import InvertedPendulum
 from scipy.integrate import solve_ivp
-# Pendulum and Cart system.
-# Y : [ x, x_dot, theta, theta_dot]
-# returns expression for Y_dot.
+
 def func2( t, y ):
-    g = 9.8 # Gravitational Acceleration
-    L = 1.5 # Length of pendulum
-    m = 1.0 #mass of bob (kg)
-    M = 5.0  # mass of cart (kg)
+    g = 9.8 
+    L = 1.5 
+    m = 1.0
+    M = 5.0 
     x_ddot = L * y[3]*y[3] * np.cos( y[2] )  -  g * np.cos(y[2]) *  np.sin(y[2])
     x_ddot = m / ( m* np.sin(y[2])* np.sin(y[2]) - M -m ) * x_ddot
     theta_ddot = -g/L * np.cos( y[2] ) - 1./L * np.sin( y[2] ) * x_ddot
@@ -17,13 +15,9 @@ def func2( t, y ):
     damping_x =  - 1.0*y[1]
     return [ y[1], x_ddot + damping_x, y[3], theta_ddot + damping_theta ]
     pass
-# Both cart and the pendulum can move.
+
 if __name__=="__main__":
-    # We need to write the Euler-lagrange equations for the both the
-    # systems (bob and cart). The equations are complicated expressions. Although
-    # it is possible to derive with hand. The entire notes are in media folder or the
-    # blog post for this entry. Otherwse in essense it is very similar to free_fall_pendulum.py
-    # For more comments see free_fall_pendulum.py
+
     sol = solve_ivp(func2, [0, 20], [ -1.0, 0., np.pi/2 + 0.1, 0. ],   t_eval=np.linspace( 0, 20, 300)  )
     syst = InvertedPendulum()
 
