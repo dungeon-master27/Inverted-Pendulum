f8c6133fbdbf0db7ee703b66fdc4347d1b916132
Vishikh <59769860+dungeon-master27@users.noreply.github.com>
2022-04-23 14:20:41 +0530

    Update forced_cart_and_pendulum.py

--- forced_cart_and_pendulum.py
+++ forced_cart_and_pendulum.py
@@ -1,10 +1,7 @@
 import numpy as np
 import cv2
-
 from free_fall_pendulum  v import InvertedPendulum
-
 from scipy.integrate import solve_ivp
-
 
 def u( t ):
     t1 = 3.
@@ -21,19 +18,14 @@ def u( t ):
 def func3( t, y ):
     g = 9.8 # Gravitational Acceleration
     L = 1.5 # Length of pendulum
-
     m = 1.0 #mass of bob (kg)
     M = 5.0  # mass of cart (kg)
 
-
     x_ddot = u(t) - m*L*y[3]*y[3] * np.cos( y[2] ) + m*g*np.cos(y[2]) *  np.sin(y[2])
     x_ddot = x_ddot / ( M+m-m* np.sin(y[2])* np.sin(y[2]) )
-
     theta_ddot = -g/L * np.cos( y[2] ) - 1./L * np.sin( y[2] ) * x_ddot
-
     damping_theta =  - 0.5*y[3]
     damping_x =  - 1.0*y[1]
-
     return [ y[1], x_ddot + damping_x, y[3], theta_ddot + damping_theta ]
 
 
@@ -45,10 +37,7 @@ if __name__=="__main__":
     # blog post for this entry. Otherwse in essense it is very similar to free_fall_pendulum.py
     # For more comments see free_fall_pendulum.py
     sol = solve_ivp(func3, [0, 20], [ -1.0, 0., np.pi/2 - 0.1, 0. ],   t_eval=np.linspace( 0, 20, 300)  )
-
-
     syst = InvertedPendulum()
-
     for i, t in enumerate(sol.t):
         rendered = syst.step( [sol.y[0,i], sol.y[1,i], sol.y[2,i], sol.y[3,i] ], t )
         cv2.imshow( 'im', rendered )
